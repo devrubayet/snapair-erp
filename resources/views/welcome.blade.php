1d06e59b2bc718fb7aee6005f6633787165f53da
rubu <rubayethossain2003@gmail.com>
2026-09-02 18:29:00 +0600

    add count on exclusive

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -47,111 +47,113 @@
     </section>
 
     <!-- exclusive-offer -->
-    <section class="exlusive-offer bg-gray-100 py-20">
-        <div class="wrapper max-w-7xl mx-auto p-4">
-            <h2 class="text-xl max-w-fit border-l-4 bg-red-100 md:text-3xl text-red-500 font-bold p-2">Exclusive Offer</h2>
+     @if (isset($offers) && count($offers) > 0)
+        <section class="exlusive-offer bg-gray-100 py-20">
+            <div class="wrapper max-w-7xl mx-auto p-4">
+                <h2 class="text-xl max-w-fit border-l-4 bg-red-100 md:text-3xl text-red-500 font-bold p-2">Exclusive Offer</h2>
 
-            <div class="crousal m-2 p-4 text-white">
-                <div class="swiper mySwiper w-full">
-                    <div class="swiper-wrapper">
-                        @foreach ($offers as $offer)
-                            <div class="swiper-slide h-[168px] w-[357.333px]">
-                                <a class="group block" href="#" target="_blank">
-                                    <div class="relative h-[168px] rounded-lg overflow-hidden">
-                                        <!-- Background Image -->
-                                        <img class="w-full h-full object-cover" src="{{ asset('storage/' . $offer->img) }}"
-                                            alt="" />
+                <div class="crousal m-2 p-4 text-white">
+                    <div class="swiper mySwiper w-full">
+                        <div class="swiper-wrapper">
+                            @foreach ($offers as $offer)
+                                <div class="swiper-slide h-[168px] w-[357.333px]">
+                                    <a class="group block" href="#" target="_blank">
+                                        <div class="relative h-[168px] rounded-lg overflow-hidden">
+                                            <!-- Background Image -->
+                                            <img class="w-full h-full object-cover" src="{{ asset('storage/' . $offer->img) }}"
+                                                alt="" />
 
-                                        <!-- Indigo Slide-up Overlay -->
-                                        <div
-                                            class="absolute inset-0 bg-red-900 text-white translate-y-full group-hover:translate-y-0 transition-all duration-500 ease-out px-5 py-4 flex flex-col justify-center">
-                                            <!-- LEFT TOP SVG -->
-                                            <div class="absolute top-0 left-0">
-                                                <svg width="133" height="108" viewBox="0 0 133 108" fill="none"
-                                                    xmlns="http://www.w3.org/2000/svg">
-                                                    <g clip-path="url(#clip0_14_14749)">
-                                                        <path
-                                                            d="M130.555 16.6804C130.555 65.2815 91.1556 104.68 42.554 104.68C-6.04747 104.68 -45.4468 65.2815 -45.4468 16.6804C-45.4468 -31.9206 -6.04747 -71.3196 42.554 -71.3196C91.1556 -71.3196 130.555 -31.9206 130.555 16.6804ZM-19.0465 16.6804C-19.0465 50.7012 8.53299 78.2804 42.554 78.2804C76.5751 78.2804 104.155 50.7012 104.155 16.6804C104.155 -17.3403 76.5751 -44.9196 42.554 -44.9196C8.53299 -44.9196 -19.0465 -17.3403 -19.0465 16.6804Z"
-                                                            fill="url(#paint0_linear_14_14749)"></path>
-                                                    </g>
-                                                    <defs>
-                                                        <linearGradient id="paint0_linear_14_14749" x1="-1.44636"
-                                                            y1="-43.8196" x2="77.5533" y2="97.681"
-                                                            gradientUnits="userSpaceOnUse">
-                                                            <stop stop-color="white" stop-opacity="0.16"></stop>
-                                                            <stop offset="1" stop-color="white" stop-opacity="0.04">
-                                                            </stop>
-                                                            <stop offset="1" stop-color="white" stop-opacity="0">
-                                                            </stop>
-                                                        </linearGradient>
-                                                        <clipPath id="clip0_14_14749">
-                                                            <rect width="132.001" height="108" fill="white"></rect>
-                                                        </clipPath>
-                                                    </defs>
-                                                </svg>
-                                            </div>
+                                            <!-- Indigo Slide-up Overlay -->
+                                            <div
+                                                class="absolute inset-0 bg-red-900 text-white translate-y-full group-hover:translate-y-0 transition-all duration-500 ease-out px-5 py-4 flex flex-col justify-center">
+                                                <!-- LEFT TOP SVG -->
+                                                <div class="absolute top-0 left-0">
+                                                    <svg width="133" height="108" viewBox="0 0 133 108" fill="none"
+                                                        xmlns="http://www.w3.org/2000/svg">
+                                                        <g clip-path="url(#clip0_14_14749)">
+                                                            <path
+                                                                d="M130.555 16.6804C130.555 65.2815 91.1556 104.68 42.554 104.68C-6.04747 104.68 -45.4468 65.2815 -45.4468 16.6804C-45.4468 -31.9206 -6.04747 -71.3196 42.554 -71.3196C91.1556 -71.3196 130.555 -31.9206 130.555 16.6804ZM-19.0465 16.6804C-19.0465 50.7012 8.53299 78.2804 42.554 78.2804C76.5751 78.2804 104.155 50.7012 104.155 16.6804C104.155 -17.3403 76.5751 -44.9196 42.554 -44.9196C8.53299 -44.9196 -19.0465 -17.3403 -19.0465 16.6804Z"
+                                                                fill="url(#paint0_linear_14_14749)"></path>
+                                                        </g>
+                                                        <defs>
+                                                            <linearGradient id="paint0_linear_14_14749" x1="-1.44636"
+                                                                y1="-43.8196" x2="77.5533" y2="97.681"
+                                                                gradientUnits="userSpaceOnUse">
+                                                                <stop stop-color="white" stop-opacity="0.16"></stop>
+                                                                <stop offset="1" stop-color="white" stop-opacity="0.04">
+                                                                </stop>
+                                                                <stop offset="1" stop-color="white" stop-opacity="0">
+                                                                </stop>
+                                                            </linearGradient>
+                                                            <clipPath id="clip0_14_14749">
+                                                                <rect width="132.001" height="108" fill="white"></rect>
+                                                            </clipPath>
+                                                        </defs>
+                                                    </svg>
+                                                </div>
 
-                                            <!-- RIGHT BOTTOM SVG -->
-                                            <div class="absolute bottom-0 right-0">
-                                                <svg width="90" height="90" viewBox="0 0 90 90" fill="none"
-                                                    xmlns="http://www.w3.org/2000/svg">
-                                                    <g clip-path="url(#clip0_14_14745)">
-                                                        <circle cx="55.8447" cy="111.899" r="58.7617"
-                                                            fill="url(#paint0_linear_14_14745)" fill-opacity="0.7">
-                                                        </circle>
-                                                        <circle cx="100.246" cy="66.2156" r="63.972"
-                                                            fill="url(#paint1_linear_14_14745)" fill-opacity="0.7">
-                                                        </circle>
-                                                    </g>
-                                                    <defs>
-                                                        <linearGradient id="paint0_linear_14_14745" x1="26.4639"
-                                                            y1="71.5005" x2="79.2159" y2="165.987"
-                                                            gradientUnits="userSpaceOnUse">
-                                                            <stop stop-color="white" stop-opacity="0.08"></stop>
-                                                            <stop offset="1" stop-color="white" stop-opacity="0">
-                                                            </stop>
-                                                        </linearGradient>
-                                                        <linearGradient id="paint1_linear_14_14745" x1="68.2604"
-                                                            y1="22.2349" x2="125.69" y2="125.099"
-                                                            gradientUnits="userSpaceOnUse">
-                                                            <stop stop-color="white" stop-opacity="0.08"></stop>
-                                                            <stop offset="1" stop-color="white" stop-opacity="0">
-                                                            </stop>
-                                                        </linearGradient>
-                                                        <clipPath id="clip0_14_14745">
-                                                            <rect width="90" height="90" fill="white"></rect>
-                                                        </clipPath>
-                                                    </defs>
-                                                </svg>
-                                            </div>
+                                                <!-- RIGHT BOTTOM SVG -->
+                                                <div class="absolute bottom-0 right-0">
+                                                    <svg width="90" height="90" viewBox="0 0 90 90" fill="none"
+                                                        xmlns="http://www.w3.org/2000/svg">
+                                                        <g clip-path="url(#clip0_14_14745)">
+                                                            <circle cx="55.8447" cy="111.899" r="58.7617"
+                                                                fill="url(#paint0_linear_14_14745)" fill-opacity="0.7">
+                                                            </circle>
+                                                            <circle cx="100.246" cy="66.2156" r="63.972"
+                                                                fill="url(#paint1_linear_14_14745)" fill-opacity="0.7">
+                                                            </circle>
+                                                        </g>
+                                                        <defs>
+                                                            <linearGradient id="paint0_linear_14_14745" x1="26.4639"
+                                                                y1="71.5005" x2="79.2159" y2="165.987"
+                                                                gradientUnits="userSpaceOnUse">
+                                                                <stop stop-color="white" stop-opacity="0.08"></stop>
+                                                                <stop offset="1" stop-color="white" stop-opacity="0">
+                                                                </stop>
+                                                            </linearGradient>
+                                                            <linearGradient id="paint1_linear_14_14745" x1="68.2604"
+                                                                y1="22.2349" x2="125.69" y2="125.099"
+                                                                gradientUnits="userSpaceOnUse">
+                                                                <stop stop-color="white" stop-opacity="0.08"></stop>
+                                                                <stop offset="1" stop-color="white" stop-opacity="0">
+                                                                </stop>
+                                                            </linearGradient>
+                                                            <clipPath id="clip0_14_14745">
+                                                                <rect width="90" height="90" fill="white"></rect>
+                                                            </clipPath>
+                                                        </defs>
+                                                    </svg>
+                                                </div>
 
-                                            <!-- DETAILS -->
-                                            <h4 class="font-bold text-xl title">{{ $offer->title }}</h4>
-                                            <p class="text-sm mt-1 desc">
-                                                {{ $offer->short_desc }}
-                                            </p>
-                                            <div class="text-xs mt-2 opacity-80 underline">
-                                                View Details
+                                                <!-- DETAILS -->
+                                                <h4 class="font-bold text-xl title">{{ $offer->title }}</h4>
+                                                <p class="text-sm mt-1 desc">
+                                                    {{ $offer->short_desc }}
+                                                </p>
+                                                <div class="text-xs mt-2 opacity-80 underline">
+                                                    View Details
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </a>
-                            </div>
-                        @endforeach
+                                    </a>
+                                </div>
+                            @endforeach
 
 
+                        </div>
+                        <div class="swiper-pagination custom-pagination relative"></div>
                     </div>
-                    <div class="swiper-pagination custom-pagination relative"></div>
                 </div>
-            </div>
 
-            <div class="offer-add mt-8 mx-3 p-7">
-                <div class="ads-img w-full h-32 rounded-md overflow-hidden">
-                    {{-- <img class="w-full h-full object-f" src="img/showcase.png" alt="" /> --}}
+                <div class="offer-add mt-8 mx-3 p-7">
+                    <div class="ads-img w-full h-32 rounded-md overflow-hidden">
+                        {{-- <img class="w-full h-full object-f" src="img/showcase.png" alt="" /> --}}
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
+    @endif
 
     <!-- airlines list -->
     <section class="airlines bg-red-100 py-24">
